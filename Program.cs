1f091e53e6b5a634bf032d28bf22df9c7263f54d
Niewiaro <Niewiaro@DESKTOP-1M2U8EO>
2022-02-25 23:06:39 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
