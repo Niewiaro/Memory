8a02ec61f0cccc737892c381f47008ea0f130847
Kuba <niewiarowski@student.agh.edu.pl>
2022-02-26 00:27:24 +0100

    difficulty level selection

--- Program.cs
+++ Program.cs
@@ -2,8 +2,14 @@
 
 namespace Memory
 {
-    class Program
+    class Matrix
     {
+        public Matrix(int level, string[] words)
+        {
+            Console.WriteLine(theLongestWord(words));
+            //int height; int width;
+        }
+
         static int theLongestWord(string[] lines)
         {
             int max = 0;
@@ -14,18 +20,43 @@ namespace Memory
                     max = line.Length;
                 }
 
-                Console.WriteLine(line);
+                Console.WriteLine(line); //just to be sure
                 Console.WriteLine("\t" + line.Length);
             }
             return max;
         }
+    }
 
-
+    class Program
+    {
         static void Main(string[] args)
         {
-            string[] lines = System.IO.File.ReadAllLines("Words.txt");
+            string file = "Words.txt";
+            string[] lines = System.IO.File.ReadAllLines(file);
+            int difficulty = 0;
 
-            Console.WriteLine(theLongestWord(lines));
+            Console.WriteLine("Welcome to memory game!\n");
+            Console.WriteLine("Select difficulty level:\n1 - Easy\t\t2 - Hard");
+
+            do
+            {
+                var input = Console.ReadKey();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        difficulty = 1;
+                        break;
+                    case ConsoleKey.D2:
+                        difficulty = 2;
+                        break;
+                    default:
+                        Console.WriteLine("\nChoose one of the available options");
+                        break;
+                }
+            } while (difficulty == 0);
+            Console.WriteLine(difficulty);
+            //Matrix Memory = new Matrix(dificulty, lines);
             
         }
     }
