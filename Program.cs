6830773e4fce539ce70ca0997bf261aa92e38dbd
Kuba <niewiarowski@student.agh.edu.pl>
2022-02-25 23:50:46 +0100

    looking for length of the longest word in txt file

--- Program.cs
+++ Program.cs
@@ -4,9 +4,29 @@ namespace Memory
 {
     class Program
     {
+        static int theLongestWord(string[] lines)
+        {
+            int max = 0;
+            foreach (string line in lines)
+            {
+                if (line.Length > max)
+                {
+                    max = line.Length;
+                }
+
+                Console.WriteLine(line);
+                Console.WriteLine("\t" + line.Length);
+            }
+            return max;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = System.IO.File.ReadAllLines("Words.txt");
+
+            Console.WriteLine(theLongestWord(lines));
+            
         }
     }
 }
